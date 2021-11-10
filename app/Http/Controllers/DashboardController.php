a0e0859da7229735aeaa69ff8575a5c7b4c95f94
Lowie Anthonissen <lowie@giveme.media>
2021-11-10 18:41:24 +0100

    feat: Rainfall on dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,13 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Weather;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('dashboard');
+        //$rainfalls = DB::table('weather')->orderBy('date', 'desc')->paginate(10);
+        $rainfalls = Weather::orderBy('date', 'desc')->paginate(10); //Puts it in a LengthAwarePaginator
+        return view('dashboard', [
+            'rainfalls' => $rainfalls
+        ]);
+
     }
 
 
